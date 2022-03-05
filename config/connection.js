0fceb77ee8a554d211f44c7f990e1b6c62f63a2c
sepideh <sepideh.ayani@target.com>
2022-03-05 14:45:14 -0600

    fixed the connection

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ const sequelize = process.env.JAWSDB_URL
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PW,
-        {
-            host: '127.0.0.1',
-            dialect: 'mysql',
-            port: 3306,
+        process.env.DB_PW, {
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
         },
-    );
+    });
 
 module.exports = sequelize;
