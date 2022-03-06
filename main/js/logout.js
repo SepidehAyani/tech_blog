f70c04fa7eb78d3fc5d7dcae037c1ed56f38ac97
sepideh <sepideh.ayani@target.com>
2022-03-06 17:21:50 -0600

    fixing logout bug

--- main/js/logout.js
+++ main/js/logout.js
@@ -1,4 +1,4 @@
-async function logout() {
+const logout = async () => {
   const response = await fetch('/api/user/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' }
